bd38ebc6049818af35596e28f503d251da8ae71e
Shane <shanemcneil19@gmail.com>
2019-04-27 15:59:45 -0700

    updated event delegation in remove function

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
     $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
 });
 
-$('.shopping-item-delete').on('click', function(event) {
+$('.shopping-list').on('click', ".shopping-item-delete", function(event) {
     $(this).closest('li').remove();
 });
   
